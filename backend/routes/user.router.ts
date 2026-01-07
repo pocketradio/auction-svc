e4de8c81fa6cdb4dc479a14fde0258be681848ab
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-01-07 11:45:05 +0530

    added a /me api route to handle jwt verification for client requests

--- backend/routes/user.router.ts
+++ backend/routes/user.router.ts
@@ -1,6 +1,14 @@
 import { Router } from "express";
+import type { Request,Response, NextFunction } from "express";
+import passport from "passport";
 
 const userRouter = Router();
 
+
+userRouter.get("/",
+    passport.authenticate("jwt", { session: false }), // calls next implicitly within
+    (req : Request, res : Response, next : NextFunction) => {
+        res.status(200).json(req.user); // passport sets the req.user if exists
+})
 
 export { userRouter };
