6ba20fb382db2908a553c8068e166253a41eb3ff
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-01-15 17:43:59 +0530

    add logout component and css fixes

--- frontend/app/(dashboard)/layout.tsx
+++ frontend/app/(dashboard)/layout.tsx
@@ -3,7 +3,8 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/ui/app-sidebar";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { ModeToggle } from "@/components/ui/mode-toggle";
-
+import { Toaster } from "@/components/ui/sonner";
+import Logout from "@/components/logout";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,13 +21,17 @@ export default function RootLayout({
           <SidebarProvider>
             <div className="flex min-h-screen w-full"> {/* flex row to keep sidebar and mainsection in 2 vertical stacks next to each other.*/}
               <AppSidebar />
-              <main className="flex-1 w-full"> {/* this is the section on the right side of the sidebar */}
-                <SidebarTrigger />
-                <ModeToggle />
+              <main className="flex flex-col flex-1 w-full"> {/* this is the section on the right side of the sidebar */}
+                <div className="flex">
+                  <SidebarTrigger />
+                  <ModeToggle />
+                  <Logout />
+                </div>
                 {children}
               </main>
             </div>
           </SidebarProvider>
+          <Toaster richColors />
         </ThemeProvider>
 
       </body>
