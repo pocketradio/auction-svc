436d4349fb892187344e80195d1cfc840cdec9ad
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-01-18 23:59:02 +0530

    feat : publish bid and auction updates to redis

--- backend/queue/consumer.ts
+++ backend/queue/consumer.ts
@@ -2,7 +2,7 @@ import { rabbit } from "./client.js";
 import { client, sha1 } from "../redis/client.js";
 import updateBid from "../services/bid.service.js";
 
-type consumerMessage = {
+type bidMessage = {
     auctionId: number,
     userId: number,
     email: string,
@@ -10,38 +10,78 @@ type consumerMessage = {
     timestamp: Date
 };
 
+type auctionMessage = {
+    auctionId: number,
+    userId: number,
+    timestamp: Date,
+}
+
+
 export const sub = rabbit.createConsumer({
     queue: 'bid_workers',
     queueOptions: { durable: true },
     exchanges: [{ exchange: 'bid', type: 'direct' }],
     qos: { prefetchCount: 1 },
-    queueBindings: [{ exchange: 'bid', routingKey: 'bid.created' }]
+    queueBindings: [{
+        exchange: 'bid', routingKey: 'bid.created'
+    }, {
+        exchange: 'bid', routingKey: 'auction.created'
+    }]
+
 }, async (msg) => {
 
-    console.log("received bid ", msg.body);
+    if (msg.routingKey === 'bid.created') {
 
-    const { auctionId, userId, email, amount, timestamp } = msg.body as consumerMessage;
+        console.log("received bid ", msg.body);
+        const { auctionId, userId, email, amount, timestamp } = msg.body as bidMessage;
 
-    //redis pipeline
-    const result = await client.evalSha(sha1, {
-        keys: [`auction:${auctionId}:currentBid`],
-        arguments: [String(userId), email, String(amount), String(timestamp)]
-    })
+        const result = await client.evalSha(sha1, {
+            keys: [`auction:${auctionId}:currentBid`],
+            arguments: [String(userId), email, String(amount), String(timestamp)]
+        })
 
-    if (result === 1) {
-        try {
-            await updateBid(auctionId, amount, userId);
+
+        // client , and not subclient is used here because subclient is for listening only ( once the subscribing starts ).
+        // client -> handles publishing ( talking ) , subClient -> listening for updates to send to SSE. 
+
+        if (result === 1) {
+            try {
+                await updateBid(auctionId, amount, userId);
+            }
+            catch (e) {
+                throw new Error("Failed to update database.");
+            }
+            await client.publish("AUCTION_UPDATES", JSON.stringify({
+                type: "BID_CREATED",
+                auctionId,
+                amount,
+                userId
+                //handle with res.success = true
+                // no need userID since response 1 handles a toast in clientside. 
+            }))
         }
-        catch (e) {
-            throw new Error("Failed to update database.");
-        }
 
-        // stream success / bid created.
+        else if (result === -1) {
+            await client.publish("AUCTION_UPDATES", JSON.stringify({
+                type: "BID_REJECTED",
+                auctionId,
+                userId
+                // handle with res.success = False
+            }))
+        }
     }
 
-    else if (result === -1) {
-        // lost bid SSE / bid failed.
+    else if (msg.routingKey === 'auction.created') {
+        // directly sent to redis with AUCTION_CREATED
+        const { auctionId, userId, timestamp } = msg.body as auctionMessage;
+        await client.publish("AUCTION_UPDATES", JSON.stringify({
+            type: "AUCTION_CREATED",
+            auctionId,
+            timestamp,
+            userId
+        }))
     }
+
 })
 
 sub.on('error', (err) => {
